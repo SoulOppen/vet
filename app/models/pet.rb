f6c2ea7a2e32c858afb7508adc9fa135c42bc5d7
oppen <arieloppenheimer@outlook.cl>
2022-05-11 12:47:35 -0400

    funciones

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,36 @@ class Pet < ApplicationRecord
     def q_history
         self.pet_histories.count
     end
+    def mean_height
+        c=self.pet_histories.count
+        if c>0
+            (self.pet_histories.inject(0){|sum,s| sum+s.height})/c
+        else
+            "N/A"
+        end
+    end
+    def mean_weight
+        c=self.pet_histories.count
+        if c>0
+            (self.pet_histories.inject(0){|sum,s| sum+s.weight})/c
+        else
+            "N/A"
+        end
+    end
+    def max_height
+        c=self.pet_histories.count
+        if c>0
+            self.pet_histories.map{|c| c.height}.max
+        else
+            "N/A"
+        end
+    end
+    def max_weight
+        c=self.pet_histories.count
+        if c>0
+            self.pet_histories.map{|c| c.weight}.max
+        else
+            "N/A"
+        end
+    end
 end
