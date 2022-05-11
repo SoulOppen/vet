931d723bfc0e98fa4feb4f535a863ab941f8485a
oppen <arieloppenheimer@outlook.cl>
2022-05-11 12:51:41 -0400

    Scaffold client

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :pet_histories
   resources :pets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
